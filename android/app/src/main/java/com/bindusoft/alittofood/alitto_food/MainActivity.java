a488ca9c6279495db1848ef9c00d5d942c93e215
sukhtarasujit <sujit.sukhtara@gmail.com>
2022-03-03 12:52:45 +0600

    03-03-22

---
+++ android/app/src/main/java/com/bindusoft/alittofood/alitto_food/MainActivity.java
@@ -0,0 +1,6 @@
+package com.bindusoft.alittofood.alitto_food;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+import 'home_page.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.dark
+      ));
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alitto Food',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white
+      ),
+      home: const HomePage(),
+    );
+  }
+
+
+}
